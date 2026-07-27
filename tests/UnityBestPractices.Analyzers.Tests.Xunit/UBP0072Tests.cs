53c85e7c0a84b6b2ce495db14a9c219e1fadf8c8
Saeed Barari <barari.saeed.am@gmail.com>
2026-07-27 17:17:41 +0330

    Add edge case coverage for advanced Unity rules

--- tests/UnityBestPractices.Analyzers.Tests.Xunit/UBP0072Tests.cs
+++ tests/UnityBestPractices.Analyzers.Tests.Xunit/UBP0072Tests.cs
@@ -28,4 +28,114 @@ public sealed class UBP0072Tests
                 .WithLocation(0)
                 .WithArguments("values"));
     }
+
+    [Fact]
+    public async Task ReportsExplicitlyTypedUnusedPersistentAllocation()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create() { NativeArray<int> {|#0:values|} = new NativeArray<int>(1, Allocator.Persistent); }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.UndisposedPersistentNativeContainer, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("values"));
+    }
+
+    [Fact]
+    public async Task ReportsNamedAllocatorArgument()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create() { var {|#0:values|} = new NativeArray<int>(length: 1, allocator: Allocator.Persistent); }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.UndisposedPersistentNativeContainer, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("values"));
+    }
+
+    [Theory]
+    [InlineData("Temp")]
+    [InlineData("TempJob")]
+    public async Task IgnoresTemporaryAllocator(string allocator)
+    {
+        var source = $$"""
+                using Unity.Collections;
+                class Owner { void Create() { var values = new NativeArray<int>(1, Allocator.{{allocator}}); } }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresPersistentAllocationThatIsDisposed()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create()
+                    {
+                        var values = new NativeArray<int>(1, Allocator.Persistent);
+                        values.Dispose();
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresPersistentAllocationPassedElsewhere()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Consume(NativeArray<int> values) { }
+                    void Create()
+                    {
+                        var values = new NativeArray<int>(1, Allocator.Persistent);
+                        Consume(values);
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresUsingDeclaration()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create() { using var values = new NativeArray<int>(1, Allocator.Persistent); }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresMultipleDeclarators()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create()
+                    {
+                        NativeArray<int> first = new NativeArray<int>(1, Allocator.Persistent), second = default;
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 }
--- tests/UnityBestPractices.Analyzers.Tests.Xunit/UBP0073Tests.cs
+++ tests/UnityBestPractices.Analyzers.Tests.Xunit/UBP0073Tests.cs
@@ -28,4 +28,162 @@ public sealed class UBP0073Tests
                 .WithLocation(0)
                 .WithArguments("TempJob"));
     }
+
+    [Theory]
+    [InlineData("Temp")]
+    [InlineData("TempJob")]
+    public async Task ReportsTemporaryLocalReturned(string allocator)
+    {
+        var source = $$"""
+                using Unity.Collections;
+                class Owner
+                {
+                    NativeArray<int> Create()
+                    {
+                        var values = new NativeArray<int>(8, Allocator.{{allocator}});
+                        return {|#0:values|};
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.InvalidTemporaryAllocatorEscape, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments(allocator));
+    }
+
+    [Fact]
+    public async Task ReportsTemporaryAllocationStoredInField()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    NativeArray<int> values;
+                    void Create() { values = {|#0:new NativeArray<int>(8, Allocator.Temp)|}; }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.InvalidTemporaryAllocatorEscape, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("Temp"));
+    }
+
+    [Fact]
+    public async Task ReportsTemporaryLocalStoredInProperty()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    NativeArray<int> Values { get; set; }
+                    void Create()
+                    {
+                        var local = new NativeArray<int>(8, Allocator.TempJob);
+                        Values = {|#0:local|};
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.InvalidTemporaryAllocatorEscape, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("TempJob"));
+    }
+
+    [Fact]
+    public async Task ReportsTemporaryCapturedByReturnedLambda()
+    {
+        var source = """
+                using System;
+                using Unity.Collections;
+                class Owner
+                {
+                    Func<int> Create()
+                    {
+                        var values = new NativeArray<int>(8, Allocator.Temp);
+                        return {|#0:() => values.Length|};
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.InvalidTemporaryAllocatorEscape, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("Temp"));
+    }
+
+    [Fact]
+    public async Task ReportsTemporaryCapturedByDelegateStoredInField()
+    {
+        var source = """
+                using System;
+                using Unity.Collections;
+                class Owner
+                {
+                    Func<int> callback;
+                    void Create()
+                    {
+                        var values = new NativeArray<int>(8, Allocator.TempJob);
+                        callback = {|#0:() => values.Length|};
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(
+            source,
+            new DiagnosticResult(DiagnosticIds.InvalidTemporaryAllocatorEscape, Microsoft.CodeAnalysis.DiagnosticSeverity.Info)
+                .WithLocation(0)
+                .WithArguments("TempJob"));
+    }
+
+    [Fact]
+    public async Task IgnoresPersistentAllocationReturn()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    NativeArray<int> Create() => new NativeArray<int>(8, Allocator.Persistent);
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresTemporaryLocalReassignedBeforeReturn()
+    {
+        var source = """
+                using Unity.Collections;
+                class Owner
+                {
+                    NativeArray<int> Create()
+                    {
+                        var values = new NativeArray<int>(8, Allocator.Temp);
+                        values = new NativeArray<int>(8, Allocator.Persistent);
+                        return values;
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task IgnoresTemporaryCapturedByNonEscapingLocalDelegate()
+    {
+        var source = """
+                using System;
+                using Unity.Collections;
+                class Owner
+                {
+                    void Create()
+                    {
+                        var values = new NativeArray<int>(8, Allocator.Temp);
+                        Func<int> callback = () => values.Length;
+                        callback();
+                    }
+                }
+                """ + "\n" + TestSources.Collections;
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 }
